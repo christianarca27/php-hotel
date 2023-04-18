0547bb8361254c9718e6d5d51daa54b0de1dde64
christianarca27 <christian.arca.mail@gmail.com>
2023-04-18 14:26:58 +0200

    feat: stampa in tabella tramite Bootstrap degli hotel con tutti i dati disponibili

--- index.php
+++ index.php
@@ -50,10 +50,72 @@ $hotels = [
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotel</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
 </head>
 
 <body>
+    <h1>PHP Hotel</h1>
 
+    <table class="table">
+        <thead>
+            <tr>
+                <?php
+                // Ciclo il primo oggetto dell'array(sarebbe andato bene uno qualsiasi) per prendere i nomi delle chiavi da inserire in testa alla tabella
+                foreach ($hotels[0] as $key => $value) {
+                    ?>
+
+                    <th scope="col">
+                        <?php
+                        echo $key;
+                        ?>
+                    </th>
+
+                    <?php
+                }
+                ?>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            // Tramite due cicli annidati:
+            // - con il primo scorro tutti gli oggetti hotel
+            // - con il secondo all'interno di ogni oggetto hotel leggo il valore di ognuno dei campi presenti
+            foreach ($hotels as $hotel) {
+                ?>
+
+                <tr>
+                    <?php
+                    foreach ($hotel as $key => $value) {
+                        ?>
+
+                        <td>
+                            <?php
+                            // Nel caso in cui stia leggendo il campo con chiave "parking" associo i valori (true, false) alla stampa di (yes, no)
+                            // Altrimenti stampo il valore così come è
+                            if ($key == "parking") {
+                                if ($value) {
+                                    echo "yes";
+                                } else {
+                                    echo "no";
+                                }
+                            } else {
+                                echo $value;
+                            }
+                            ?>
+                        </td>
+
+                        <?php
+                    }
+                    ?>
+                </tr>
+
+                <?php
+            }
+            ?>
+        </tbody>
+    </table>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
