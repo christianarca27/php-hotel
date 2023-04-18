f6ccbc655be4179815da32b12799c9b49e3b6b3f
christianarca27 <christian.arca.mail@gmail.com>
2023-04-18 15:33:54 +0200

    feat: stilizzazione tramite bootstrap

--- index.php
+++ index.php
@@ -58,97 +58,102 @@ $numResults = 0;
 </head>
 
 <body>
-    <h1>PHP Hotel</h1>
+    <div class="container">
+        <h1>PHP Hotel</h1>
 
-    <form action="index.php" method="get">
-        <label for="checkbox-parking">Parcheggio disponibile</label>
-        <input type="checkbox" name="parking" id="checkbox-parking" <?php echo $isParkingFilterActive == "on" ? "checked" : ""; ?>>
+        <form class="border border-1 p-3" action="index.php" method="get">
+            <h2>Filtri</h2>
 
-        <br>
+            <div class="form-check form-switch mb-3">
+                <label class="form-check-label" for="checkbox-parking">Parcheggio disponibile</label>
+                <input class="form-check-input" type="checkbox" name="parking" id="checkbox-parking" <?php echo $isParkingFilterActive == "on" ? "checked" : ""; ?> aria-describedby="basic-addon1">
+            </div>
 
-        <label for="input-vote">Voto</label>
-        <input type="number" min="0" name="vote" id="input-vote" placeholder="Inserisci voto minimo" <?php echo $minVote > 0 ? "value='{$minVote}'" : "" ?>>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon1">Voto</span>
+                <input class="form-control" type="number" min="0" name="vote" id="input-vote"
+                    placeholder="Inserisci voto minimo" <?php echo $minVote > 0 ? "value='{$minVote}'" : "" ?>>
+            </div>
 
-        <br>
+            <button type="submit" class="btn btn-primary">Filtra risultati</button>
+        </form>
 
-        <input type="submit" value="Filtra">
-    </form>
-
-    <table class="table">
-        <thead>
-            <tr>
-                <?php
-                // Ciclo il primo oggetto dell'array(sarebbe andato bene uno qualsiasi) per prendere i nomi delle chiavi da inserire in testa alla tabella
-                foreach ($hotels[0] as $key => $value) {
-                    ?>
-
-                    <th scope="col">
-                        <?php
-                        echo $key;
-                        ?>
-                    </th>
-
+        <table class="table">
+            <thead>
+                <tr>
                     <?php
+                    // Ciclo il primo oggetto dell'array(sarebbe andato bene uno qualsiasi) per prendere i nomi delle chiavi da inserire in testa alla tabella
+                    foreach ($hotels[0] as $key => $value) {
+                        ?>
+
+                        <th scope="col">
+                            <?php
+                            echo $key;
+                            ?>
+                        </th>
+
+                        <?php
+                    }
+                    ?>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                // Tramite due cicli annidati:
+                // - con il primo scorro tutti gli oggetti hotel
+                // - con il secondo all'interno di ogni oggetto hotel leggo il valore di ognuno dei campi presenti
+                foreach ($hotels as $hotel) {
+                    // Considera l'hotel solo se:
+                    // - non è attivo nessun filtro
+                    // - è attivo il filtro sul parcheggio e l'hotel ha il parcheggio
+                    // - il voto minimo dell'hotel è >= a quello impostato nel filtro di ricerca (in caso non fosse presente 0)
+                    if (($isParkingFilterActive == "off" || ($isParkingFilterActive == "on" && $hotel["parking"] == "yes")) && $hotel["vote"] >= $minVote) {
+                        $numResults++;
+                        ?>
+
+                        <tr>
+                            <?php
+                            foreach ($hotel as $key => $value) {
+                                ?>
+
+                                <td>
+                                    <?php
+                                    // Nel caso in cui stia leggendo il campo con chiave "parking" associo i valori (true, false) alla stampa di (yes, no)
+                                    // Altrimenti stampo il valore così come è
+                                    if ($key == "parking") {
+                                        if ($value) {
+                                            echo "yes";
+                                        } else {
+                                            echo "no";
+                                        }
+                                    } else {
+                                        echo $value;
+                                    }
+                                    ?>
+                                </td>
+
+                                <?php
+                            }
+                            ?>
+                        </tr>
+
+                        <?php
+                    }
                 }
                 ?>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            // Tramite due cicli annidati:
-            // - con il primo scorro tutti gli oggetti hotel
-            // - con il secondo all'interno di ogni oggetto hotel leggo il valore di ognuno dei campi presenti
-            foreach ($hotels as $hotel) {
-                // Considera l'hotel solo se:
-                // - non è attivo nessun filtro
-                // - è attivo il filtro sul parcheggio e l'hotel ha il parcheggio
-                // - il voto minimo dell'hotel è >= a quello impostato nel filtro di ricerca (in caso non fosse presente 0)
-                if (($isParkingFilterActive == "off" || ($isParkingFilterActive == "on" && $hotel["parking"] == "yes")) && $hotel["vote"] >= $minVote) {
-                    $numResults++;
-                    ?>
+            </tbody>
+        </table>
 
-                    <tr>
-                        <?php
-                        foreach ($hotel as $key => $value) {
-                            ?>
-
-                            <td>
-                                <?php
-                                // Nel caso in cui stia leggendo il campo con chiave "parking" associo i valori (true, false) alla stampa di (yes, no)
-                                // Altrimenti stampo il valore così come è
-                                if ($key == "parking") {
-                                    if ($value) {
-                                        echo "yes";
-                                    } else {
-                                        echo "no";
-                                    }
-                                } else {
-                                    echo $value;
-                                }
-                                ?>
-                            </td>
-
-                            <?php
-                        }
-                        ?>
-                    </tr>
-
-                    <?php
-                }
-            }
-            ?>
-        </tbody>
-    </table>
-
-    <?php
-    // In caso ci fosse almeno un risultato stampo il numero di risultati ottenuti dalla ricerca
-    // Altrimenti avviso l'utente che non ci sono risultati corrispondenti con i criteri selezionati
-    if ($numResults > 0) {
-        echo "<p>Risultati trovati: {$numResults}</p>";
-    } else {
-        echo "<p>Non è stato possibile trovare risultati con i criteri selezionati.</p>";
-    }
-    ?>
+        <?php
+        // In caso ci fosse almeno un risultato stampo il numero di risultati ottenuti dalla ricerca
+// Altrimenti avviso l'utente che non ci sono risultati corrispondenti con i criteri selezionati
+        if ($numResults > 0) {
+            echo "<p>Risultati trovati: {$numResults}</p>";
+        } else {
+            echo "<p>Non è stato possibile trovare risultati con i criteri selezionati.</p>";
+        }
+        ?>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
